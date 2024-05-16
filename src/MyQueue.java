153a4c0bb5ecd35e1640a608827c9a73cfe5642f
kvn147 <kevin458377@gmail.com>
2024-05-15 19:33:13 -0700

    Finished MyQueue

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -7,6 +7,46 @@ isEmpty()         Returns true if the queue has no elements
 size()            Returns the number of elements in the queue
 */
 
+import java.util.*;
 
+public class MyQueue<E> {
+
+    private ArrayList<E> queue;
+
+    public MyQueue() {
+        queue = new ArrayList<>();
+    }
+
+    public void add(E value) {
+        queue.add(value);
+    }
+
+    public E remove() {
+        if (queue.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        return queue.remove(0);
+    }
+
+    public E peek() {
+        if (queue.isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.get(0);
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public String toString() {
+        return queue.toString();
+    }
+}
 
 
